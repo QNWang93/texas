9ede923c1cab7d51ce9bbc4c14e984935f687506
qinan <qwang75@jhu.edu>
2020-02-05 12:22:28 -0500

    add batch tool

---
+++ website/batch.py
@@ -0,0 +1,10 @@
+import lc_ex
+import sys
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    f = open(filename, 'r')
+    for line in f:
+        para = line.split(' ')
+        para[3] = para[3][:4]
+        lc_ex.main(para)
