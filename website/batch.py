35a202b2b2aea27467fd35ee6ffe7069e29ed5b9
qinan <qwang75@jhu.edu>
2020-03-27 17:26:09 -0400

    X

--- website/batch.py
+++ website/batch.py
@@ -1,5 +1,7 @@
 import lc_ex
+import texas
 import sys
+from astropy.io import ascii
 
 def main(filename):
 #    filename = arg[0]
@@ -9,6 +11,15 @@ def main(filename):
         print(para)
         para[3] = para[3][:4]
         lc_ex.main(para)
-
+        
+def main_texas(filename):
+    t = ascii.read(filename)
+    for i in t:
+        try:
+            galcan = texas.main([i['RA'], i['Dec'], '3', './plots/'+name + '_texas'])
+            ascii.write(galcan, home_dir+name+'_texas.txt', overwrite=True)  
+        else:
+            print('unable to access PanSTARRS for '+i['Name'])
+            
 if __name__ == "__main__":
     main(sys.argv[1])
